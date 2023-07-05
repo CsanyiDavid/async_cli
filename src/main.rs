0e19bb39d23560c5f6f1b7f1abf9f85a2cc4c593
Csanyi David <csanyidavid2000@gmail.com>
2023-07-05 11:56:09 +0200

    read_from_file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io, thread::spawn};
+#![allow(dead_code, unused_variables)]
+
+use std::{io, env};
+use tokio;
 use tokio::time::{sleep, Duration};
 
 #[tokio::main]
@@ -13,6 +16,7 @@ async fn main() {
         }
         println!("input {} ", input);
         tokio::spawn(sleeper(5, input));
+        tokio::spawn(read_from_file("./file.txt".into()));
     }
 
 }
@@ -21,4 +25,17 @@ async fn sleeper(secs: u64, name: String) {
     println!("{} sleeper started with {} secs", name, secs);
     sleep(Duration::from_secs(secs)).await;
     println!("{} slept {} secs", name, secs);
+}
+
+async fn read_from_file(path: String) {
+    println!("Read file {}", path);
+    let read_result = tokio::fs::read_to_string(path.clone()).await;
+    match read_result {
+        Ok(file_content) => {
+            println!("Reading file {} done. Content: {}", path, file_content);
+        }
+        Err(e) => {
+            println!("An error occured during reading file {}. Error: {}", path, e);
+        }
+    }
 }
