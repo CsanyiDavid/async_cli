4ac684d4409014a6632f7d1eb9c1289b442deeef
Csanyi David <csanyidavid2000@gmail.com>
2023-07-05 11:29:45 +0200

    start async sleeper after read

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-use std::io;
+use std::{io, thread::spawn};
+use tokio::time::{sleep, Duration};
 
-fn main() {
-    let mut input = String::new();
+#[tokio::main]
+async fn main() {
     let stdin = io::stdin();
-    stdin.read_line(&mut input);
-    println!("input: {}", input);
+    let mut input = String::new();
+    loop {
+        input = "".to_string();
+        stdin.read_line(&mut input).unwrap();
+        if input.ends_with('\n') {
+            input.truncate(input.len()-1);
+        }
+        println!("input {} ", input);
+        tokio::spawn(sleeper(5, input));
+    }
+
+}
+
+async fn sleeper(secs: u64, name: String) {
+    println!("{} sleeper started with {} secs", name, secs);
+    sleep(Duration::from_secs(secs)).await;
+    println!("{} slept {} secs", name, secs);
 }
